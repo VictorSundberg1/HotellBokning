df5d24df698cf7ff85241714e64f4af1db7d5e7e
VictorSundberg1 <victor.sundberg1@hotmail.com>
2025-11-10 12:50:41 +0100

    Added function to delete booking and update room availability. Also added iam role env variable

---
+++ functions/deleteBookings/index.js
@@ -0,0 +1,85 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const {
+	DeleteCommand,
+	DynamoDBDocumentClient,
+	GetCommand,
+	UpdateCommand,
+} = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({});
+const db = DynamoDBDocumentClient.from(client);
+
+exports.handler = async (event, context) => {
+	const { bookingNr } = event.pathParameters;
+
+	try {
+		const getBookingCommand = new GetCommand({
+			TableName: 'booking-table',
+			Key: { id: bookingNr },
+		});
+
+		const result = await db.send(getBookingCommand);
+
+		if (!result.Item) {
+			return {
+				statusCode: 404,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					success: false,
+					message: 'Booking not found',
+				}),
+			};
+		}
+		// Kollar efter både lista "roomIds" och ensam string "roomId"
+		const roomIds = Array.isArray(result.Item.roomIds)
+			? result.Item.roomIds
+			: [result.Item.roomId];
+
+		const deleteCommand = new DeleteCommand({
+			TableName: 'booking-table',
+			Key: { id: bookingNr },
+		});
+
+		await db.send(deleteCommand);
+
+		//Uppdaterar alla roomIds som plockats ut och sätter available till true
+		const updatePromises = roomIds.map((roomId) => {
+			const updateRoomCommand = new UpdateCommand({
+				TableName: 'rooms-table',
+				Key: { id: roomId },
+				UpdateExpression: 'SET isAvailable = :available',
+				ExpressionAttributeValues: {
+					':available': true,
+				},
+			});
+			return db.send(updateRoomCommand);
+		});
+		//Väntar på att alla uppdateringar är klara
+		await Promise.all(updatePromises);
+
+		return {
+			statusCode: 200,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				success: true,
+				message: 'Booking deleted and availability updated',
+			}),
+		};
+	} catch (error) {
+		console.error('Error:', error);
+		return {
+			statusCode: 500,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				success: false,
+				error: error.message,
+			}),
+		};
+	}
+};
